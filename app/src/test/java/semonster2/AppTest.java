305e7a070505d310eb3c938227332411642c7f04
qilin <102580690+hkqilin@users.noreply.github.com>
2023-07-03 10:00:51 +0900

    Appのテストを追加

--- app/src/test/java/semonster2/AppTest.java
+++ app/src/test/java/semonster2/AppTest.java
@@ -3,12 +3,14 @@
  */
 package semonster2;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class AppTest {
-    @Test public void appHasAGreeting() {
+    @Test
+    public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertEquals("こんにちは SEMonster", classUnderTest.getGreeting());
     }
 }
