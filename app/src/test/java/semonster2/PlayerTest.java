fdbb5090f513417830bb6e766de46228e5d58c28
qilin <102580690+hkqilin@users.noreply.github.com>
2023-07-09 11:21:25 +0900

    テストを修正

--- app/src/test/java/semonster2/PlayerTest.java
+++ app/src/test/java/semonster2/PlayerTest.java
@@ -10,10 +10,12 @@ import org.junit.Test;
 public class PlayerTest {
 	@Test
 	public void testToString() {
-		LinkedList<Integer> l = new LinkedList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+		LinkedList<Integer> l = new LinkedList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
 		var player = new Player(l, "hoge");
 		player.drawMonsters();
 
-		assertEquals("Deck:hoge\nスライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]", player.toString());
+		assertEquals(
+				"Deck:hoge\nHP:5\nスライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]スライム:レア度[0]\n",
+				player.toString());
 	}
 }
