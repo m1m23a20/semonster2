4c559e562a61a1ef72f70c171fae8092cff41511
qilin <102580690+hkqilin@users.noreply.github.com>
2023-07-09 11:59:26 +0900

    基本機能実装

--- app/src/main/java/semonster2/SeMonsterGame.java
+++ app/src/main/java/semonster2/SeMonsterGame.java
@@ -1,4 +1,24 @@
 package semonster2;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class SeMonsterGame {
+	HashMap<String, Player> playerMap = new HashMap<>();
+	LinkedList<Integer> randomNumberList;
+
+	SeMonsterGame(LinkedList<Integer> randomNumberList) {
+		this.randomNumberList = randomNumberList;
+	}
+
+	void addPlayer(String playerName) {
+		var player = new Player(randomNumberList, playerName);
+		this.playerMap.put(playerName, player);
+	}
+
+	void draw(String playerName) {
+		var player = this.playerMap.get(playerName);
+		player.drawMonsters();
+		System.out.println(player);
+	}
 }
